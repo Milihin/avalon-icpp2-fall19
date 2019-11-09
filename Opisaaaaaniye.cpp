38767e4625182c32b0eebc3019682b96f8a12137
Milihin <55630681+Milihin@users.noreply.github.com>
2019-11-09 21:27:28 +0300

    Add files via upload

--- Opisaaaaaniye.cpp
+++ Opisaaaaaniye.cpp
@@ -1,8 +1,7 @@
 #include "Date.h"
 #include <cmath>
-#include <iostream>
+#include "stdio.h"
 #include <string>
-
 
 
 
@@ -50,12 +49,7 @@ namespace ext {
 			return "f";
 			break;
 		}
-
-
-
 	}
-
-
 	int intMonth12(Month mon) {
 		switch (mon)
 		{
@@ -99,12 +93,8 @@ namespace ext {
 			return (int)"f*ck";
 			break;
 		}
-
-
-
 	}
-	//необходимая функция, реализует нормализацию
-	Date DateToNormal(Date inpp) 
+	Date DateToNormal(Date inpp)
 	{
 		int temp, monthtemp = 0;
 		if (inpp.day > 365) {
@@ -117,13 +107,19 @@ namespace ext {
 			}
 		}
 	}
-
-
-
-
-
-
-
+	Date RJND(TimeDelta delta) {
+		Date outpp;
+		int a = delta.delta + 32044;
+		int b = ((4 * a) + 3) / 146097;
+		int c = a - ((146097 * b) / 4);
+		int d = ((4 * c) + 3) / 1461;
+		int e = c - ((1461 * d) / 4);
+		int m = ((5 * e) + 2) / 153;
+		outpp.day = e - ((153 * m + 2) / 5) + 1;
+		outpp.month = static_cast<Month>(m + 3 - 12 * (m / 10));
+		outpp.year = (100 * b) + d - 4800 + (m / 10);
+		return outpp;
+	}
 	TimeDelta countJND(Date date)
 	{
 		int a = (14 - intMonth12(date.month)) / 12;
@@ -133,33 +129,24 @@ namespace ext {
 		outpp.delta = (date.day + ((153 * m + 2) / 5) + 365 * y + (y / 4) - (y / 100) + (y / 400) - 32045);
 		return outpp;
 	}
-
-
-
-
 	Date operator - (Date a, Date b) {
 		Date per;
 		b.day - a.day;
 		intMonth12(b.month) - intMonth12(a.month);
 		b.year - a.year;
 	}
-
-
 	TimeDelta countDistance(Date from, Date to) {
 		TimeDelta outpp, a = countJND(to), b = countJND(from);
 		outpp.delta = a.delta - b.delta;
 		return outpp;
 
 	}
-
 	void print(Date data, DateFormat format)
 	{
 		std::cout << data.year << " ";
 		print(data.month, format);
 		std::cout << data.day;
 	}
-
-
 	void print(Month month, DateFormat format)
 	{
 		if (format == DateFormat::MonthAsInt)
@@ -183,7 +170,6 @@ namespace ext {
 	Season getSeason(Date date)
 	{
 		return getSeason(date.month);
-
 	}
 	Season getSeason(Month month)
 	{
@@ -204,9 +190,7 @@ namespace ext {
 		{
 			return Season::Autumn;
 		}
-
 	}
-
 	bool  operator == (const Date lhs, const Date rhs)
 	{
 		return lhs.day == rhs.day
@@ -219,9 +203,31 @@ namespace ext {
 	}
 	bool operator < (const Date lhs, const Date rhs)
 	{
-		return lhs.day < rhs.day
-			&& lhs.month < rhs.month
-			&& lhs.year < rhs.year;
+		if (lhs.year < rhs.year) {
+			return true;
+		}
+		else if (lhs.year > rhs.year) {
+			return false;
+		}
+		else {
+			if (lhs.month < rhs.month) {
+				return true;
+			}
+			else if (lhs.month > rhs.month) {
+				return false;
+			}
+			else {
+				if (lhs.day < rhs.day) {
+					return true;
+				}
+				else if (lhs.day > rhs.day) {
+					return false;
+				}
+				else {
+					return true;
+				}
+			}
+		}
 	}
 	bool operator <= (const Date lhs, const Date rhs)
 	{
@@ -229,9 +235,31 @@ namespace ext {
 	}
 	bool operator > (const Date lhs, const Date rhs)
 	{
-		return lhs.day > rhs.day
-			&& lhs.month > rhs.month
-			&& lhs.year > rhs.year;
+		if (lhs.year > rhs.year) {
+			return true;
+		}
+		else if (lhs.year < rhs.year) {
+			return false;
+		}
+		else {
+			if (lhs.month > rhs.month) {
+				return true;
+			}
+			else if (lhs.month < rhs.month) {
+				return false;
+			}
+			else {
+				if (lhs.day > rhs.day) {
+					return true;
+				}
+				else if (lhs.day < rhs.day) {
+					return false;
+				}
+				else {
+					return true;
+				}
+			}
+		}
 	}
 	bool operator >= (const Date lhs, const Date rhs)
 	{
@@ -249,36 +277,254 @@ namespace ext {
 		temp.day = date.day + delta.delta;
 		return DateToNormal(temp);
 	}
-	Date operator + (const TimeDelta delta, const TimeDelta date) 
+	TimeDelta operator + (const TimeDelta delta, const TimeDelta date)
 	{
 		Date outpp;
 		outpp.day = delta.delta + date.delta;
-		return DateToNormal(outpp);
+		return countJND(outpp);
 	}
-	Date operator - (const Date date, const TimeDelta delta) 
+	Date operator - (const Date date, const TimeDelta delta)
 	{
 		Date temp = date;
 		temp.day = date.day - delta.delta;
 		return DateToNormal(temp);
 	}
-	Date operator - (const TimeDelta delta, const Date date) {
+	Date operator - (const TimeDelta delta, const Date date) 
+	{
 		Date temp;
 		TimeDelta tempp = countJND(date);
 
 		temp.day = delta.delta - tempp.delta;
 		return DateToNormal(temp);
 	}
-	Date operator - (const TimeDelta delta, const TimeDelta date) 
+	Date operator - (const TimeDelta delta, const Date date) 
 	{
+		TimeDelta one = countJND(date);
+		return RJND(one - delta);
+	}
+	Date operator - (Date a, Date b) {
+		Date per;
+		b.day - a.day;
+		abs(intMonth12(b.month) - intMonth12(a.month));
+		b.year - a.year;
+	}
+	TimeDelta operator - (const TimeDelta lhs, const TimeDelta rhs) 
+	{
+		TimeDelta one;
+		one.delta = lhs.delta - rhs.delta;
+		return one;
+	}
+	TimeDelta operator * (const TimeDelta delta, int multiplier) {
+		TimeDelta one;
+		one.delta = (delta.delta * multiplier);
+		return one;
+	}
+	TimeDelta operator / (const TimeDelta delta, int multiplier) {
+		TimeDelta one;
+		one.delta = (delta.delta / multiplier);
+		return one;
+	}
+	TimeDelta operator ++ (TimeDelta& delta)
+	{
+		++delta.delta;
+		return delta;
+	}
+	TimeDelta operator ++ (TimeDelta& delta, int)
+	{
+		TimeDelta temp = delta;
+		++delta.delta;
+		return temp;
+	}
+	TimeDelta operator -- (TimeDelta& delta)
+	{
+		--delta.delta;
+		return delta;
+	}
+	TimeDelta operator -- (TimeDelta& delta, int)
+	{
+
+		TimeDelta temp = delta;
+		--delta.delta;
+		return temp;
+	}
+	Date operator ++ (Date& delta)
+	{
+		delta.day++;
+		delta.year++;
+		delta.month = static_cast<Month>(static_cast<int>(delta.month) + 1);
+		return delta;
+	}
+	Date operator ++ (Date& delta, int)
+	{
+		delta.day++;
+		delta.year++;
+		delta.month = static_cast<Month>(static_cast<int>(delta.month) + 1);
+		return delta;
+	}
+	Date operator -- (Date& delta)
+	{
+		delta.day--;
+		delta.year--;
+		delta.month = static_cast<Month>(static_cast<int>(delta.month) - 1);
+		return delta;
+	}
+	Date operator -- (Date& delta, int)
+	{
+		delta.day--;
+		delta.year--;
+		delta.month = static_cast<Month>(static_cast<int>(delta.month) - 1);
+		return delta;
+	}
+	TimeDelta countDistance(Date from, Date to) {
+		Date tempp;
+		tempp = to - from;
+		TimeDelta outpp = { ((tempp.day + (intMonth12(tempp.month) * 30) + (tempp.year * 365))) };
+		return outpp;
+	}
+	void swap(Date& lhs, Date& rhs) {
+		Date swap;
+		swap = lhs;
+		lhs = rhs;
+		rhs = swap;
+	}
+	void swap(TimeDelta& lhs, TimeDelta& rhs) {
+		TimeDelta swap = lhs;
+		lhs = rhs;
+		rhs = swap;
+	}
+	Date& max(Date& lhs,  Date& rhs) {
+		return lhs > rhs ? lhs : rhs;
+
+
+		if (lhs.year > rhs.year) {
+			return lhs;
+		}
+		else if (lhs.year < rhs.year) {
+			return rhs;
+		}
+		else {
+			if (lhs.month > rhs.month) {
+				return lhs;
+			}
+			else if (lhs.month < rhs.month) {
+				return rhs;
+			}
+			else {
+				if (lhs.day > rhs.day) {
+					return lhs;
+				}
+				else if (lhs.day < rhs.day) {
+					return rhs;
+				}
+				else {
+					return rhs;
+				}
+			}
+		}
+	}
+	Date& min(Date& lhs,  Date& rhs) {
+
+		if (lhs.year > rhs.year) {
+			return rhs;
+		}
+		else if (lhs.year < rhs.year) {
+			return lhs;
+		}
+		else {
+			if (lhs.month > rhs.month) {
+				return rhs;
+			}
+			else if (lhs.month < rhs.month) {
+				return lhs;
+			}
+			else {
+				if (lhs.day > rhs.day) {
+					return rhs;
+				}
+				else if (lhs.day < rhs.day) {
+					return lhs;
+				}
+				else {
+					return rhs;
+				}
+
+			}
+		}
+	}
+
+	Date& getMinDate(Date dates[], int size) {
+		Date * minn = &dates[0];
+		for (int i = 0; i < size; i++) {
+			if (*minn > dates[i]) {
+				minn = &dates[i];
+			}
+		}
+		return *minn;
+	}
+
+	Date& getManDate(Date dates[], int size) {
+		Date *  miax = &dates[0];
+		for (int i = 0; i < size; i++) {
+			if (*miax < dates[i]) {
+				miax = &dates[i];
+			}
+		}
+		return *miax;
+	}
+	void sort(Date dates[], int size, SortBy sortBy = SortBy::Date) {
+		Date temp;
+		bool stopper = false;
+		if (sortBy == SortBy::Season) {
+			for (int i = 0; i < size - 1; i++) {
+				if (stopper) {
+					stopper = false;
+					i = 0;
+				}
+				if (getSeason(dates[i]) > getSeason(dates[i + 1])) {
+					stopper = true;
+					temp = dates[i];
+					dates[i] = dates[i + 1];
+					dates[i + 1] = temp;
+				}
+			}
+		}
+		else if (sortBy == SortBy::Date) {
+			for (int i = 0; i < size - 1; i++) {
+				if (stopper) {
+					stopper = false;
+					i = 0;
+				}
+				if (dates[i] > dates[i + 1]) {
+					stopper = true;
+					temp = dates[i];
+					dates[i] = dates[i + 1];
+					dates[i + 1] = temp;
+				}
+			}
+		}
+		else {
+			throw "f*ck";
+		}
+
+	}
+
+
+		Date* tryFillDate(int year, int month, int day)
+		{
+			Date * TF3 = new Date();
+			TF3->day = day;
+			TF3->month = static_cast<Month>(month);
+			TF3->year = year;
+			if (TF3 == DateToNormal(TF3)) {
+				return TF3;
+			}
+			else {
+				return nullptr;
+			}
+		}
 
 
 
 
 	}
-
-
-
-	
 }
-
-
