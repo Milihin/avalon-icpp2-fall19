ce4f5c706012f35be3f44fc268968e30a7c79cc6
Milihin <55630681+Milihin@users.noreply.github.com>
2019-11-30 21:23:18 +0300

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -16,20 +16,31 @@ namespace xxx {
 			tmp->nextSymbol = ptr;
 		}
 	}
-
-
-	void print(charString* str) {
-		
+	void print(charString* str) {	
 		while (str != nullptr) {
 			std::cout << str->symbol;
 			str = str->nextSymbol;
 		}
+	}
 
+	void removeChars(fuck* str, int start, int ent) {
+		fuck* savestr = str;
+		int count = 0;
+		while (count != start) {
+			str = str->nextSymbol;
+			count++;
+		}
+
+		fuck* temp = str->nextSymbol;
+		for (int i = 0; i > ent; i++) {
+			delete str;
+			str = temp;
+			temp = str->nextSymbol;
+			
+		}
 
 
 	}
-
-
 
 
 
--- charptrrr.h
+++ charptrrr.h
@@ -10,9 +10,12 @@ namespace xxx {
 		char symbol;
 		charString* nextSymbol;
 	};
-
+	
+	using fuck = charString;
 	void append(charString* &str, char symbol);
 	void print(charString* str);
-
+	/* функци€ принимает три аргумента : динамическую строку и два числа.‘ункци€ удал€ет заданное количество символов(третий аргумент) с заданной позиции(второй аргумент) динамической строки.
+	Ќапример, если динамическа€ строка - 'abcdefg', второй агрумент - 2, третий - 3. “о после работы функции исходна€ динамическа€ строка должна стать 'aefg'.*/
+	void removeChars(fuck str, int start, int ent);
 
 }
